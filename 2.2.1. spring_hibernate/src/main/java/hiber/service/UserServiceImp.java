5e76655a0e6f697bc6dd83fdb687d69d3d657d88
Ruslan <ruslan.rust@gmail.com>
2023-12-15 01:32:37 +0300

    2.2.1 v.2

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -11,9 +11,12 @@ import java.util.List;
 @Service
 public class UserServiceImp implements UserService {
 
-   @Autowired
-   private UserDao userDao;
+   private final UserDao userDao;
 
+   @Autowired
+   public UserServiceImp(UserDao userDao) {
+      this.userDao = userDao;
+   }
    @Transactional
    @Override
    public void add(User user) {
